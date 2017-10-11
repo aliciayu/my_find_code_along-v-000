fb7b19e7c7be5e1b9dfa1e557a6d79a085166922
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-11 22:42:43 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < 100
-    yield(1)
-      i = 0 + 1
-      binding.pry
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
   end
 end
 
